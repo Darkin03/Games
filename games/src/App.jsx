7279fad880bc8f2106ae7793401234a8d0949f5c
Darkin03 <esnaiderortega@unisinu.edu.co>
2024-01-12 22:20:01 -0500

    beta

---
+++ games/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <header className='header'>
+        <h1>Games</h1>
+      </header>
+      <main className='content'>
+        <div className='game'></div>
+        <div className='game'></div>
+        <div className='game'></div>
+        <div className='game'></div>
+        <div className='game'></div>
+        <div className='game'></div>
+
+
+      </main>
+
+      <footer>
+        Esnaider David Ortega Rodriguez
+      </footer>
+    </div>
+     
+  )
+}
+
+export default App
